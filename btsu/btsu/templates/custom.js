bc8c83749b3d22595e1990e7d315a34f3d9d924d
Tony Su <peitaosu@163.com>
2017-11-30 23:39:00 +0800

    add progress bar for searching

--- btsu/btsu/templates/custom.js
+++ btsu/btsu/templates/custom.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $("#search_warning").hide();
+    $("#progress_bar").hide();
     $("#search_btn").click(function(){
         $("#search_warning").show();
+        $("#progress_bar").show();
     });
 });
