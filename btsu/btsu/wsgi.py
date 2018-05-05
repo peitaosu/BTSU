9541df6a8ab2f8cf26f6292acc6327bb41933270
Tony Su <peitaosu@163.com>
2018-05-05 23:37:11 +0800

    fix issue: btsu.settings module not found

--- btsu/btsu/wsgi.py
+++ btsu/btsu/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+app_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if app_path not in sys.path:
+    sys.path.append(app_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "btsu.settings")
 
